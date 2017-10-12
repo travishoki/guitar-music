bfd28874e4a0bc01a11f1042b6622e78a4581e69
Travis Hoki <travis.hoki@gmail.com>
2017-10-12 17:38:58 -0600

    not found page

--- src/components/common/Main.js
+++ src/components/common/Main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import HomePage from '../pages/home/HomePage';
 import SongPage from '../pages/song/SongPage';
+import NotFoundPage from '../pages/notFound/NotFoundPage';
+
 
 import { Switch, Route } from 'react-router-dom'
 const Main = () => (
@@ -8,6 +10,7 @@ const Main = () => (
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route path='/song/:title' component={SongPage}/>
+            <Route path='*' component={NotFoundPage} />
         </Switch>
     </main>
 );
