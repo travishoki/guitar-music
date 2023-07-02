2e235eddb7eee748101d6405ec44c505d1eb9615
Travis Hoki <travis.hoki@gmail.com>
2023-07-02 10:39:09 -0600

    alias lyric list

---
+++ src/components/lyrics/index.js
@@ -0,0 +1,71 @@
+import AllThroughTheNight from "../components/lyrics/AllThroughTheNight";
+import AmericaTheBeautiful from "../components/lyrics/AmericaTheBeautiful";
+import BabyBaluga from "../components/lyrics/BabyBaluga";
+import BabyILoveYourWay from "../components/lyrics/BabyILoveYourWay";
+import BlowinInTheWind from "../components/lyrics/BlowinInTheWind";
+import CountryRoads from "../components/lyrics/CountryRoads";
+import Edelweiss from "../components/lyrics/Edelweiss";
+import FiveHundredMiles from "../components/lyrics/FiveHundredMiles";
+import GodBlessTheUSA from "../components/lyrics/GodBlessTheUSA";
+import HesGotTheWholeWorldInHisHands from "../components/lyrics/HesGotTheWholeWorldInHisHands";
+import LeavingOnAJetPlane from "../components/lyrics/LeavingOnAJetPlane";
+import LetThereBePeaceOnEarch from "../components/lyrics/LetThereBePeaceOnEarch";
+import MichaelFinnegan from "../components/lyrics/MichaelFinnegan";
+import MichaelRowTheBoatAshore from "../components/lyrics/MichaelRowTheBoatAshore";
+import Mockingbird from "../components/lyrics/Mockingbird";
+import MorningHasBroken from "../components/lyrics/MorningHasBroken";
+import NoahsArk from "../components/lyrics/NoahsArk";
+import OneTinSoldier from "../components/lyrics/OneTinSoldier";
+import OverTheRainbow from "../components/lyrics/OverTheRainbow";
+import PuffTheMagicDragon from "../components/lyrics/PuffTheMagicDragon";
+import PutYourHandInTheHand from "../components/lyrics/PutYourHandInTheHand";
+import Seattle from "../components/lyrics/Seattle";
+import TellMeWhy from "../components/lyrics/TellMeWhy";
+import TheCatCameBack from "../components/lyrics/TheCatCameBack";
+import TheSoundsOfSilence from "../components/lyrics/TheSoundsOfSilence";
+import ThisLandIsYourLand from "../components/lyrics/ThisLandIsYourLand";
+import TieMeKangarooDownSport from "../components/lyrics/TieMeKangarooDownSport";
+import TurnTurnTurn from "../components/lyrics/TurnTurnTurn";
+import WeddingSong from "../components/lyrics/WeddingSong";
+import WhatChildIsThis from "../components/lyrics/WhatChildIsThis";
+import WhatTheWorldNeedsNowIsLove from "../components/lyrics/WhatTheWorldNeedsNowIsLove";
+import WhereHaveAllTheFlowersGone from "../components/lyrics/WhereHaveAllTheFlowersGone";
+import YellowSubmarine from "../components/lyrics/YellowSubmarine";
+import YouAreMySunshine from "../components/lyrics/YouAreMySunshine";
+
+export default {
+  AllThroughTheNight,
+  AmericaTheBeautiful,
+  BabyBaluga,
+  BabyILoveYourWay,
+  BlowinInTheWind,
+  CountryRoads,
+  Edelweiss,
+  FiveHundredMiles,
+  GodBlessTheUSA,
+  HesGotTheWholeWorldInHisHands,
+  LeavingOnAJetPlane,
+  LetThereBePeaceOnEarch,
+  MichaelFinnegan,
+  MichaelRowTheBoatAshore,
+  Mockingbird,
+  MorningHasBroken,
+  NoahsArk,
+  OneTinSoldier,
+  OverTheRainbow,
+  PuffTheMagicDragon,
+  PutYourHandInTheHand,
+  Seattle,
+  TellMeWhy,
+  TheCatCameBack,
+  TheSoundsOfSilence,
+  ThisLandIsYourLand,
+  TieMeKangarooDownSport,
+  TurnTurnTurn,
+  WeddingSong,
+  WhatChildIsThis,
+  WhatTheWorldNeedsNowIsLove,
+  WhereHaveAllTheFlowersGone,
+  YellowSubmarine,
+  YouAreMySunshine,
+};
--- src/const/SongList.js
+++ src/const/SongList.js
@@ -1,39 +1,5 @@
-import AllThroughTheNight from "../components/lyrics/AllThroughTheNight";
-import AmericaTheBeautiful from "../components/lyrics/AmericaTheBeautiful";
-import BabyBaluga from "../components/lyrics/BabyBaluga";
-import BabyILoveYourWay from "../components/lyrics/BabyILoveYourWay";
-import BlowinInTheWind from "../components/lyrics/BlowinInTheWind";
-import CountryRoads from "../components/lyrics/CountryRoads";
-import Edelweiss from "../components/lyrics/Edelweiss";
-import FiveHundredMiles from "../components/lyrics/FiveHundredMiles";
-import GodBlessTheUSA from "../components/lyrics/GodBlessTheUSA";
-import HesGotTheWholeWorldInHisHands from "../components/lyrics/HesGotTheWholeWorldInHisHands";
-import LeavingOnAJetPlane from "../components/lyrics/LeavingOnAJetPlane";
-import LetThereBePeaceOnEarch from "../components/lyrics/LetThereBePeaceOnEarch";
-import MichaelFinnegan from "../components/lyrics/MichaelFinnegan";
-import MichaelRowTheBoatAshore from "../components/lyrics/MichaelRowTheBoatAshore";
-import Mockingbird from "../components/lyrics/Mockingbird";
-import MorningHasBroken from "../components/lyrics/MorningHasBroken";
-import NoahsArk from "../components/lyrics/NoahsArk";
-import OneTinSoldier from "../components/lyrics/OneTinSoldier";
-import OverTheRainbow from "../components/lyrics/OverTheRainbow";
-import PuffTheMagicDragon from "../components/lyrics/PuffTheMagicDragon";
-import PutYourHandInTheHand from "../components/lyrics/PutYourHandInTheHand";
-import Seattle from "../components/lyrics/Seattle";
-import TellMeWhy from "../components/lyrics/TellMeWhy";
-import TheCatCameBack from "../components/lyrics/TheCatCameBack";
-import TheSoundsOfSilence from "../components/lyrics/TheSoundsOfSilence";
-import ThisLandIsYourLand from "../components/lyrics/ThisLandIsYourLand";
-import TieMeKangarooDownSport from "../components/lyrics/TieMeKangarooDownSport";
-import TurnTurnTurn from "../components/lyrics/TurnTurnTurn";
-import WeddingSong from "../components/lyrics/WeddingSong";
-import WhatChildIsThis from "../components/lyrics/WhatChildIsThis";
-import WhatTheWorldNeedsNowIsLove from "../components/lyrics/WhatTheWorldNeedsNowIsLove";
-import WhereHaveAllTheFlowersGone from "../components/lyrics/WhereHaveAllTheFlowersGone";
-import YellowSubmarine from "../components/lyrics/YellowSubmarine";
-import YouAreMySunshine from "../components/lyrics/YouAreMySunshine";
-
 import { GENRE_FOLK, GENRE_TRADITIONAL } from "./genres";
+import LYRIC_LIST from "../components/lyrics";
 
 module.exports = [
   {
@@ -41,206 +7,206 @@ module.exports = [
     artist: "Misc Traditional",
     genres: [GENRE_TRADITIONAL],
     link: "https://tabs.ultimate-guitar.com/m/misc_traditional/all_through_the_night_crd.htm",
-    lyrics: AllThroughTheNight,
+    lyrics: LYRIC_LIST.AllThroughTheNight,
   },
   {
     title: "America the Beautiful",
     artist: "Misc Traditional",
     genres: [GENRE_TRADITIONAL],
     link: "https://tabs.ultimate-guitar.com/m/misc_traditional/america_the_beautiful_crd.htm",
-    lyrics: AmericaTheBeautiful,
+    lyrics: LYRIC_LIST.AmericaTheBeautiful,
   },
   {
     title: "Baby Baluga",
     artist: "Raffi Cavoukian",
     link: "https://tabs.ultimate-guitar.com/r/raffi/baby_beluga_crd.htm",
-    lyrics: BabyBaluga,
+    lyrics: LYRIC_LIST.BabyBaluga,
   },
   {
     title: "Baby I Love Your Way",
     artist: "Peter Frampton",
     link: "https://tabs.ultimate-guitar.com/p/peter_frampton/baby_i_love_your_way_crd.htm",
-    lyrics: BabyILoveYourWay,
+    lyrics: LYRIC_LIST.BabyILoveYourWay,
   },
   {
     title: "Blowin' in the Wind",
     artist: "Bob Dylan",
     // link: "https://tabs.ultimate-guitar.com/b/bob_dylan/blowin_in_the_wind_ver10_crd_1086954id_25082011date.htm",
-    lyrics: BlowinInTheWind,
+    lyrics: LYRIC_LIST.BlowinInTheWind,
   },
   {
     title: "Country Roads",
     artist: "John Denver",
     // link: "https://tabs.ultimate-guitar.com/j/john_denver/country_roads_ver2_crd.htm",
-    lyrics: CountryRoads,
+    lyrics: LYRIC_LIST.CountryRoads,
   },
   {
     title: "Edelweiss",
     artist: "Bob Carlisle",
     // link: "https://tabs.ultimate-guitar.com/s/sound_of_music/edelweiss_crd.htm",
-    lyrics: Edelweiss,
+    lyrics: LYRIC_LIST.Edelweiss,
   },
   {
     title: "Five Hundred Miles",
     artist: "Peter, Paul & Mary",
     link: "https://tabs.ultimate-guitar.com/p/peter_paul_and_mary/500_miles_crd.htm",
-    lyrics: FiveHundredMiles,
+    lyrics: LYRIC_LIST.FiveHundredMiles,
   },
   {
     title: "God Bless the USA",
     artist: "Lee Greenwood",
     link: "https://tabs.ultimate-guitar.com/l/lee_greenwood/god_bless_the_usa_crd.htm",
-    lyrics: GodBlessTheUSA,
+    lyrics: LYRIC_LIST.GodBlessTheUSA,
   },
   {
     title: "He's Got the Whole World in His Hands",
     artist: "Misc Praise",
     link: "https://tabs.ultimate-guitar.com/m/misc_praise_songs/hes_got_the_whole_world_in_his_hands_crd.htm",
-    lyrics: HesGotTheWholeWorldInHisHands,
+    lyrics: LYRIC_LIST.HesGotTheWholeWorldInHisHands,
   },
   {
     title: "Leaving on a Jet Plane",
     artist: "John Denver",
     // link: "https://tabs.ultimate-guitar.com/j/john_denver/leaving_on_a_jetplane_ver2_crd.htm",
-    lyrics: LeavingOnAJetPlane,
+    lyrics: LYRIC_LIST.LeavingOnAJetPlane,
   },
   {
     title: "Let there be Peace on Earth",
     artist: "Vince Gill",
     link: "https://tabs.ultimate-guitar.com/v/vince_gill/let_there_be_peace_on_earth_crd.htm",
-    lyrics: LetThereBePeaceOnEarch,
+    lyrics: LYRIC_LIST.LetThereBePeaceOnEarch,
   },
   {
     title: "Michael Finnegan",
     artist: "Misc Traditional",
     // link: "https://tabs.ultimate-guitar.com/m/misc_traditional/misc_traditional_-_michael_finnigan_crd.htm",
     genres: [GENRE_TRADITIONAL],
-    lyrics: MichaelFinnegan,
+    lyrics: LYRIC_LIST.MichaelFinnegan,
   },
   {
     title: "Michael Row the Boat Ashore",
     artist: "Misc Traditional",
     // link: "https://tabs.ultimate-guitar.com/t/the_highwaymen/michael_row_the_boat_ashore_crd.htm",
     genres: [GENRE_TRADITIONAL],
-    lyrics: MichaelRowTheBoatAshore,
+    lyrics: LYRIC_LIST.MichaelRowTheBoatAshore,
   },
   {
     title: "Mockingbird",
     artist: "Misc Traditional",
     link: "https://tabs.ultimate-guitar.com/m/mother_goose/hush_little_baby_crd.htm",
-    lyrics: Mockingbird,
+    lyrics: LYRIC_LIST.Mockingbird,
   },
   {
     title: "Morning Has Broken",
     artist: "Cat Stevens",
     // link: "https://tabs.ultimate-guitar.com/c/cat_stevens/morning_has_broken_crd.htm",
-    lyrics: MorningHasBroken,
+    lyrics: LYRIC_LIST.MorningHasBroken,
   },
   {
     title: "Noah's Ark",
     artist: "Misc Children",
     link: "https://tabs.ultimate-guitar.com/m/misc_children/rise_and_shine_arky_arky_crd.htm",
-    lyrics: NoahsArk,
+    lyrics: LYRIC_LIST.NoahsArk,
   },
   {
     title: "One Tin Soldier",
     artist: "The Original Caste",
     // link: "https://tabs.ultimate-guitar.com/m/misc_unsigned_bands/the_original_caste_-_one_tin_soldier_crd.htm",
-    lyrics: OneTinSoldier,
+    lyrics: LYRIC_LIST.OneTinSoldier,
   },
   {
     title: "Over the Rainbow",
     artist: "Israel Kamakawiwo'ole",
     // link: "https://tabs.ultimate-guitar.com/i/israel_kamakawiwoole/somewhere_over_the_rainbow_crd.htm",
-    lyrics: OverTheRainbow,
+    lyrics: LYRIC_LIST.OverTheRainbow,
   },
   {
     title: "Puff The Magic Dragon",
     artist: "Peter, Paul & Mary",
     genres: [GENRE_FOLK],
     link: "https://tabs.ultimate-guitar.com/p/peter_paul_and_mary/puff_the_magic_dragon_ver3_crd.htm",
-    lyrics: PuffTheMagicDragon,
+    lyrics: LYRIC_LIST.PuffTheMagicDragon,
   },
   {
     title: "Put Your Hand in the Hand",
     artist: "Anne Murray",
     link: "https://tabs.ultimate-guitar.com/a/anne_murray/put_your_hand_in_the_hand_ver2_crd.htm",
-    lyrics: PutYourHandInTheHand,
+    lyrics: LYRIC_LIST.PutYourHandInTheHand,
   },
   {
     title: "Seattle",
     artist: "Perry Como",
     link: "http://www.classic-country-song-lyrics.com/seattlelyricschords.html",
-    lyrics: Seattle,
+    lyrics: LYRIC_LIST.Seattle,
   },
   {
     title: "Tell My Why",
     artist: "Misc Traditional",
     link: "https://tabs.ultimate-guitar.com/m/melanie/tell_me_why_crd.htm",
-    lyrics: TellMeWhy,
+    lyrics: LYRIC_LIST.TellMeWhy,
   },
   {
     title: "The Cat Came Back",
     artist: "Harry S Miller",
     link: "https://tabs.ultimate-guitar.com/h/harry_s_miller/the_cat_came_back_crd.htm",
-    lyrics: TheCatCameBack,
+    lyrics: LYRIC_LIST.TheCatCameBack,
   },
   {
     title: "The Sounds of Silence",
     artist: "Simon & Garfunkel",
     link: "https://tabs.ultimate-guitar.com/s/simon_garfunkel/the_sound_of_silence_ver4_crd.htm",
-    lyrics: TheSoundsOfSilence,
+    lyrics: LYRIC_LIST.TheSoundsOfSilence,
   },
   {
     title: "This Land is your Land",
     artist: "Woody Guthrie",
     link: "http://www.guitaretab.com/w/woody-guthrie/223617.html",
-    lyrics: ThisLandIsYourLand,
+    lyrics: LYRIC_LIST.ThisLandIsYourLand,
   },
   {
     title: "Tie Me Kangaroo Down Sport",
     artist: "Rolf Harris",
     link: "https://tabs.ultimate-guitar.com/r/rolf_harris/tie_me_kangaroo_down_sport_ver4_crd.htm",
-    lyrics: TieMeKangarooDownSport,
+    lyrics: LYRIC_LIST.TieMeKangarooDownSport,
   },
   {
     title: 'Turn" Turn" Turn',
     artist: "The Byrds",
-    lyrics: TurnTurnTurn,
+    lyrics: LYRIC_LIST.TurnTurnTurn,
   },
   {
     title: "Wedding Song",
     artist: "Paul Stookey",
-    lyrics: WeddingSong,
+    lyrics: LYRIC_LIST.WeddingSong,
   },
   {
     title: "What Child is This",
     artist: "Misc Christmas",
     link: "https://tabs.ultimate-guitar.com/m/misc_christmas/what_child_is_this_crd.htm",
-    lyrics: WhatChildIsThis,
+    lyrics: LYRIC_LIST.WhatChildIsThis,
   },
   {
     title: "What the World Needs Now is Love",
     artist: "Diana Ross",
     link: "https://tabs.ultimate-guitar.com/j/jackie_deshannon/what_the_world_needs_now_is_love_crd.htm",
-    lyrics: WhatTheWorldNeedsNowIsLove,
+    lyrics: LYRIC_LIST.WhatTheWorldNeedsNowIsLove,
   },
   {
     title: "Where Have All the Flowers Gone",
     artist: "Pete Seeger",
     link: "https://tabs.ultimate-guitar.com/p/peter_paul_and_mary/where_have_all_the_flowers_gone_ver2_crd.htm",
-    lyrics: WhereHaveAllTheFlowersGone,
+    lyrics: LYRIC_LIST.WhereHaveAllTheFlowersGone,
   },
   {
     title: "Yellow Submarine",
     artist: "The Beatles",
     link: "https://tabs.ultimate-guitar.com/t/the_beatles/yellow_submarine_ver2_crd.htm",
-    lyrics: YellowSubmarine,
+    lyrics: LYRIC_LIST.YellowSubmarine,
   },
   {
     title: "You Are My Sunshine",
     artist: "Chris Stapleton",
     // link: "https://tabs.ultimate-guitar.com/c/chris_stapleton/you_are_my_sunshine_crd.htm",
-    lyrics: YouAreMySunshine,
+    lyrics: LYRIC_LIST.YouAreMySunshine,
   },
 ];
