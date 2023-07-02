6ec800af895aa3468459232a3f4c12ab9f2933e1
Travis Hoki <travis.hoki@gmail.com>
2023-07-02 02:10:01 -0600

    dark and light mode toggle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { MuiThemeProvider } from "material-ui/styles";
-import { colors } from "material-ui/styles";
 import { BrowserRouter } from "react-router-dom";
 
 import Header from "./components/common/Header";
@@ -9,14 +8,25 @@ import Main from "./components/common/Main";
 import MuiTheme from "./components/common/MuiTheme";
 import "./styles/index.less";
 
-const App = () => (
-  <MuiThemeProvider muiTheme={MuiTheme}>
-    <div>
-      <Header />
-      <Main />
-    </div>
-  </MuiThemeProvider>
-);
+const App = () => {
+  const [isdarkMode, setIsdarkMode] = useState(true);
+
+  const onToggleIsDarkMode = () => {
+    setIsdarkMode(!isdarkMode);
+  };
+
+  return (
+    <MuiThemeProvider muiTheme={MuiTheme}>
+      <div>
+        <Header
+          isdarkMode={isdarkMode}
+          onToggleIsDarkMode={onToggleIsDarkMode}
+        />
+        <Main />
+      </div>
+    </MuiThemeProvider>
+  );
+};
 
 render(
   <BrowserRouter basename="/guitar">
