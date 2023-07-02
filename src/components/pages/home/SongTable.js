a25e61d2d589fc3e345ad2ff44f8fea9de1556f4
Travis Hoki <travis.hoki@gmail.com>
2023-07-02 03:08:51 -0600

    hook components

--- src/components/pages/home/SongTable.js
+++ src/components/pages/home/SongTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import SongList from "../../common/SongList";
@@ -6,92 +6,82 @@ import GuitarTabLink from "../../common/GuitarTabLink";
 import Genre from "./Genre/Genre";
 import { GENRE_ALL } from "./Genre/const";
 
-class SongTable extends React.Component {
-  constructor(props) {
-    super(props);
+const SongTable = () => {
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("title");
+  const [genre, setGenre] = useState(GENRE_ALL);
 
-    this.state = {
-      search: "",
-      sort: "title",
-      genre: GENRE_ALL,
-    };
-
-    this.onSelectGenre = this.onSelectGenre.bind(this);
-    this.searchSong = this.searchSong.bind(this);
-  }
-  searchSong(e) {
+  const searchSong = (e) => {
     console.log("searchSong");
-    const search = e.currentTarget.value;
-    this.setState({ search });
-  }
-  onSelectGenre(newGenre) {
-    console.log(`newGenre: ${newGenre}`);
-    this.setState({ genre: newGenre });
-  }
+    const newSearch = e.currentTarget.value;
+    setSearch(newSearch);
+  };
 
-  render() {
-    const filterTerm = this.state.search.toLowerCase();
-    const sortTerm = this.state.sort;
-    const filteredSongs = SongList.filter((song) => {
-      const title = song.title.toLowerCase();
-      return filterTerm == "" || title.indexOf(filterTerm) > -1;
+  const onSelectGenre = (newGenre) => {
+    setGenre(newGenre);
+  };
+
+  const filterTerm = search.toLowerCase();
+  const sortTerm = sort;
+  const filteredSongs = SongList.filter((song) => {
+    const title = song.title.toLowerCase();
+    return filterTerm == "" || title.indexOf(filterTerm) > -1;
+  })
+    .filter((song) => {
+      if (genre === GENRE_ALL) return true;
+      if (!song.genres) return false;
+      return song.genres.includes(genre);
     })
-      .filter((song) => {
-        if (this.state.genre === GENRE_ALL) return true;
-        if (!song.genres) return false;
-        return song.genres.includes(this.state.genre);
-      })
-      .sort((a, b) => {
-        if (a[sortTerm] < b[sortTerm]) return -1;
-        if (a[sortTerm] > b[sortTerm]) return 1;
-        return 0;
-      });
+    .sort((a, b) => {
+      if (a[sortTerm] < b[sortTerm]) return -1;
+      if (a[sortTerm] > b[sortTerm]) return 1;
+      return 0;
+    });
 
-    const tdStyle = {
-      paddingLeft: 10,
-      paddingRight: 10,
-    };
+  const tdStyle = {
+    paddingLeft: 10,
+    paddingRight: 10,
+  };
 
-    const ctrlTdStyle = {
-      paddingLeft: 10,
-      paddingRight: 10,
-      width: "32px",
-    };
+  const ctrlTdStyle = {
+    paddingLeft: 10,
+    paddingRight: 10,
+    width: "32px",
+  };
 
-    function fixUrlTitle(title) {
-      return title.replace(/ /g, "-").toLowerCase();
-    }
+  const fixUrlTitle = (title) => {
+    return title.replace(/ /g, "-").toLowerCase();
+  };
 
-    return (
-      <div>
-        <Genre currentGenre={this.state.genre} onClick={this.onSelectGenre} />
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Artist</th>
-              <th style={ctrlTdStyle}>Tabs</th>
+  return (
+    <div>
+      <Genre currentGenre={genre} onClick={onSelectGenre} />
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+            <th style={ctrlTdStyle}>Tabs</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredSongs.map((song) => (
+            <tr key={song.title}>
+              <td style={tdStyle}>
+                <Link to={"/song/" + fixUrlTitle(song.title)}>
+                  {song.title}
+                </Link>
+              </td>
+              <td style={tdStyle}>{song.artist}</td>
+              <td style={ctrlTdStyle}>
+                <GuitarTabLink song={song} />
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {filteredSongs.map((song) => (
-              <tr key={song.title}>
-                <td style={tdStyle}>
-                  <Link to={"/song/" + fixUrlTitle(song.title)}>
-                    {song.title}
-                  </Link>
-                </td>
-                <td style={tdStyle}>{song.artist}</td>
-                <td style={ctrlTdStyle}>
-                  <GuitarTabLink song={song} />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default SongTable;
--- src/components/pages/song/SongPage.js
+++ src/components/pages/song/SongPage.js
@@ -3,40 +3,31 @@ import { Link } from "react-router-dom";
 import SongList from "../../common/SongList";
 import GuitarTabLink from "../../common/GuitarTabLink";
 
-class SongPage extends React.Component {
-  constructor(props) {
-    super(props);
+const SongPage = () => {
+  const title = props.match.params.title;
 
-    const title = props.match.params.title;
-    const song = SongList.filter((songListItem) => {
-      return songListItem.title.replace(/ /g, "-").toLowerCase() === title;
-    })[0];
+  const song = SongList.filter((songListItem) => {
+    return songListItem.title.replace(/ /g, "-").toLowerCase() === title;
+  })[0];
 
-    this.state = {
-      song,
-    };
-  }
-  render() {
-    const song = this.state.song;
-    return (
-      <div id="songPage">
-        <Link to="/" className="back-button">
-          Back
-        </Link>
-        <h1>{song.title}</h1>
-        <p>by {song.artist}</p>
-        {song.lyrics && (
-          <div className="lyrics">
-            <song.lyrics />
-          </div>
-        )}
-        <div className="go-to-tabs">
-          <GuitarTabLink song={song} />
-          <p>Go to tabs</p>
+  return (
+    <div id="songPage">
+      <Link to="/" className="back-button">
+        Back
+      </Link>
+      <h1>{song.title}</h1>
+      <p>by {song.artist}</p>
+      {song.lyrics && (
+        <div className="lyrics">
+          <song.lyrics />
         </div>
+      )}
+      <div className="go-to-tabs">
+        <GuitarTabLink song={song} />
+        <p>Go to tabs</p>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SongPage;
