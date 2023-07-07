46185cf5bce630c1eaed3ffb4ac36c81c6f1e305
Travis Hoki <travis.hoki@gmail.com>
2023-07-07 05:27:39 -0600

    setup prettier

--- src/components/lyrics/BlowinInTheWind.tsx
+++ src/components/lyrics/BlowinInTheWind.tsx
@@ -2,58 +2,58 @@ import React from 'react';
 
 const BlowinInTheWind = () => (
 	<div>
-    How many roads must a man walk down
+		How many roads must a man walk down
 		<br />
-    Before you call him a man?
+		Before you call him a man?
 		<br />
-    How many seas must a white dove sail
+		How many seas must a white dove sail
 		<br />
-    Before she sleeps in the sand?
+		Before she sleeps in the sand?
 		<br />
-    Yes, and how many times must the cannon balls fly
+		Yes, and how many times must the cannon balls fly
 		<br />
-    Before they&apos;re forever banned?
-		<br />
-		<br />
-    The answer, my friend, is blowin&apos; in the wind
-		<br />
-    The answer is blowin&apos; in the wind.
+		Before they&apos;re forever banned?
 		<br />
 		<br />
-    Yes, and how many years can a mountain exist
+		The answer, my friend, is blowin&apos; in the wind
 		<br />
-    Before it&apos;s washed to the sea?
-		<br />
-    Yes, and how many years can some people exist
-		<br />
-    Before they&apos;re allowed to be free?
-		<br />
-    Yes, and how many times can a man turn his head
-		<br />
-    And pretend that he just doesn&apos;t see?
+		The answer is blowin&apos; in the wind.
 		<br />
 		<br />
-    The answer, my friend, is blowin&apos; in the wind
+		Yes, and how many years can a mountain exist
 		<br />
-    The answer is blowin&apos; in the wind.
+		Before it&apos;s washed to the sea?
 		<br />
+		Yes, and how many years can some people exist
 		<br />
-    Yes, and how many times must a man look up
+		Before they&apos;re allowed to be free?
 		<br />
-    Before he can see the sky?
+		Yes, and how many times can a man turn his head
 		<br />
-    Yes, and how many ears must one man have
-		<br />
-    Before he can hear people cry?
-		<br />
-    Yes, and how many deaths will it take &apos;til he knows
-		<br />
-    That too many people have died?
+		And pretend that he just doesn&apos;t see?
 		<br />
 		<br />
-    The answer, my friend, is blowin&apos; in the wind
+		The answer, my friend, is blowin&apos; in the wind
 		<br />
-    The answer is blowin&apos; in the wind.
+		The answer is blowin&apos; in the wind.
+		<br />
+		<br />
+		Yes, and how many times must a man look up
+		<br />
+		Before he can see the sky?
+		<br />
+		Yes, and how many ears must one man have
+		<br />
+		Before he can hear people cry?
+		<br />
+		Yes, and how many deaths will it take &apos;til he knows
+		<br />
+		That too many people have died?
+		<br />
+		<br />
+		The answer, my friend, is blowin&apos; in the wind
+		<br />
+		The answer is blowin&apos; in the wind.
 	</div>
 );
 
--- src/components/lyrics/MichaelFinnegan.tsx
+++ src/components/lyrics/MichaelFinnegan.tsx
@@ -2,37 +2,37 @@ import React from 'react';
 
 const MichaelFinnegan = () => (
 	<div>
-    There was an old man named Michael Finnegan
+		There was an old man named Michael Finnegan
 		<br />
-    He had whiskers on his chinnegan
+		He had whiskers on his chinnegan
 		<br />
-    They fell out and then grew in again
+		They fell out and then grew in again
 		<br />
-    Poor old Michael Finnegan
+		Poor old Michael Finnegan
 		<br />
-    Begin again.
-		<br />
-		<br />
-    There was an old man named Michael Finnegan
-		<br />
-    He went fishing with a pinnegan
-		<br />
-    Caught a fish and dropped it in again
-		<br />
-    Poor old Michael Finnegan
-		<br />
-    Begin again.
+		Begin again.
 		<br />
 		<br />
-    There was an old man named Michael Finnegan
+		There was an old man named Michael Finnegan
 		<br />
-    He grew fat and then grew thin again
+		He went fishing with a pinnegan
 		<br />
-    Then he died and had to begin again
+		Caught a fish and dropped it in again
 		<br />
-    Poor old Michael Finnegan
+		Poor old Michael Finnegan
 		<br />
-    Begin again.
+		Begin again.
+		<br />
+		<br />
+		There was an old man named Michael Finnegan
+		<br />
+		He grew fat and then grew thin again
+		<br />
+		Then he died and had to begin again
+		<br />
+		Poor old Michael Finnegan
+		<br />
+		Begin again.
 	</div>
 );
 
