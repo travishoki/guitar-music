83fb6c3bbeec3e0c1302ef09d642837359ab89ab
Travis Hoki <travis.hoki@gmail.com>
2023-07-16 13:36:37 -0600

    comment divide song imports

--- src/components/lyrics/index.tsx
+++ src/components/lyrics/index.tsx
@@ -1,82 +1,116 @@
+// - B -
 import BabyBaluga from './BabyBaluga';
 import BabyILoveYourWay from './BabyILoveYourWay';
 import BackHome from './BackHome';
 import BlowinInTheWind from './BlowinInTheWind';
 import ButterflyKisses from './ButterflyKisses';
+// - C -
 import ClosingTimeChords from './ClosingTimeChords';
 import CountryRoads from './CountryRoads';
+// - F -
 import FreeFallin from './FreeFallin';
+// - G -
 import GivingTree from './GivingTree';
 import GodBlessTheUSA from './GodBlessTheUSA';
 import GoodAsYou from './GoodAsYou';
+// - H -
 import HesGotTheWholeWorldInHisHands from './HesGotTheWholeWorldInHisHands';
 import HouseAtPoohCorner from './HouseAtPoohCorner';
+// - I -
 import ICantMakeYouLoveMe from './ICantMakeYouLoveMe';
 import ItsYourLove from './ItsYourLove';
+// - J -
 import Jumper from './Jumper';
+// - L -
 import LeavingOnAJetPlane from './LeavingOnAJetPlane';
+// - M -
 import MakeYouFeelMyLove from './MakeYouFeelMyLove';
 import MichaelFinnegan from './MichaelFinnegan';
 import MichaelRowTheBoatAshore from './MichaelRowTheBoatAshore';
 import Mockingbird from './Mockingbird';
+// - N -
 import NightChanges from './NightChanges';
+// - O -
 import OneTinSoldier from './OneTinSoldier';
 import OverTheRainbow from './OverTheRainbow';
+// - P -
 import PuffTheMagicDragon from './PuffTheMagicDragon';
+// - R -
 import RightHereWaitingChords from './RightHereWaitingChords';
+// - S -
 import Sunshine from './Sunshine';
+// - T -
 import TearsInHeaven from './TearsInHeaven';
 import TheCatCameBack from './TheCatCameBack';
 import TheSoundsOfSilence from './TheSoundsOfSilence';
 import ThisLandIsYourLand from './ThisLandIsYourLand';
 import TieMeKangarooDownSport from './TieMeKangarooDownSport';
 import TurnTurnTurn from './TurnTurnTurn';
+// - W -
 import WalkingOnSunshine from './WalkingOnSunshine';
 import WhatTheWorldNeedsNowIsLove from './WhatTheWorldNeedsNowIsLove';
 import WhereHaveAllTheFlowersGone from './WhereHaveAllTheFlowersGone';
 import WithOrWithoutYou from './WithOrWithoutYou';
+// - Y -
 import YellowSubmarine from './YellowSubmarine';
 import YouAreMySunshine from './YouAreMySunshine';
 import YoureStillTheOne from './YoureStillTheOne';
 
 export default {
+	// - B -
 	BabyBaluga,
 	BabyILoveYourWay,
 	BackHome,
 	BlowinInTheWind,
 	ButterflyKisses,
+	// - C -
 	ClosingTimeChords,
 	CountryRoads,
+	// - F -
 	FreeFallin,
+	// - G -
 	GivingTree,
 	GodBlessTheUSA,
 	GoodAsYou,
+	// - H -
 	HesGotTheWholeWorldInHisHands,
 	HouseAtPoohCorner,
+	// - I -
 	ICantMakeYouLoveMe,
 	ItsYourLove,
+	// - J -
 	Jumper,
+	// - L -
 	LeavingOnAJetPlane,
+	// - M -
 	MakeYouFeelMyLove,
 	MichaelFinnegan,
 	MichaelRowTheBoatAshore,
 	Mockingbird,
+	// - N -
 	NightChanges,
+	// - O -
 	OneTinSoldier,
 	OverTheRainbow,
+	// - P -
 	PuffTheMagicDragon,
+	// - R -
 	RightHereWaitingChords,
+	// - S -
 	Sunshine,
+	// - T -
 	TearsInHeaven,
 	TheCatCameBack,
 	TheSoundsOfSilence,
 	ThisLandIsYourLand,
 	TieMeKangarooDownSport,
 	TurnTurnTurn,
+	// - W -
 	WalkingOnSunshine,
 	WhatTheWorldNeedsNowIsLove,
 	WhereHaveAllTheFlowersGone,
 	WithOrWithoutYou,
+	// - Y -
 	YellowSubmarine,
 	YouAreMySunshine,
 	YoureStillTheOne,
