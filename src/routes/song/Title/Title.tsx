5cfc5cab320b76ae33b63586e43dd8f1f628019b
Travis Hoki <travis.hoki@gmail.com>
2023-07-25 21:03:40 -0600

    scroll-header styling

--- src/routes/song/Title/Title.tsx
+++ src/routes/song/Title/Title.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Title = ({ title }: TitleProps) => <h2>{title}</h2>;
+const Title = ({ title }: TitleProps) => (
+	<h2 className="scroll-header" style={titleStyle}>
+		{title}
+	</h2>
+);
 
 type TitleProps = {
 	title: string;
+};
+
+const titleStyle: React.CSSProperties = {
+	position: 'sticky',
+	top: 50,
 };
 
 export default Title;
