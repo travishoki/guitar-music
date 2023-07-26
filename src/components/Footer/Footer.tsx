074aacb982eff0d01c6ec3623ebc44df1210a241
Travis Hoki <travis.hoki@gmail.com>
2023-07-25 20:49:19 -0600

    footer only back button if not root

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
+import { useLocation } from 'react-router-dom';
+
 import ScrollToTopButton from './ScrollToTopButton/ScrollToTopButton';
 import BackButton from '../../routes/song/BackButton/BackButton';
 
-const Footer = () => (
-	<footer>
-		<div className="max-container" style={footerStyle}>
-			<BackButton />
-			<ScrollToTopButton />
-		</div>
-	</footer>
-);
+const Footer = () => {
+	const location = useLocation();
+
+	const isRootPath = location.pathname === '/';
+
+	return (
+		<footer>
+			<div className="max-container" style={footerStyle}>
+				{!isRootPath && <BackButton />}
+				<ScrollToTopButton />
+			</div>
+		</footer>
+	);
+};
 
 const footerStyle: React.CSSProperties = {
 	alignItems: 'center',
