cff1fb3b92f95ff81276021e162d38556118baf7
Travis Hoki <travis.hoki@gmail.com>
2026-08-03 17:55:10 -0600

    update script

--- docs/ultimate-guitar-print.js
+++ docs/ultimate-guitar-print.js
@@ -1,9 +1,22 @@
+// Only keep the body
+const mainContent = '.LLGvZ.BqjAv'
+const keep = document.querySelector(mainContent);
+if (keep) document.body.replaceChildren(keep.cloneNode(true));
+
 // Remove Elements
 [
-	'.Tdc9E', '.S-wLP', '.JxP4w', '.D3YWx', '.MW-Pf',
-	'._66LiL', '.h7uZ6', '.NLKCx', '.n04zq', '._-27s-',
-	'.Vzz2Z', '.XkJho', '._aGHQ', '.fqEMR', '.Lf02O:nth-child(2)', '.ZxtwW.ZoZpf.S7mHx.FI6Wy',
-    '.UtR6x', '.eU82V.aFGkI._7YwBw'
+    '.JxP4w', // Floating Right Controls
+    '.Vzz2Z', // Floating Right Controls
+    '._66LiL', // Comments Section
+	'.NLKCx', // Top Section Menu
+    '.n04zq', // UGC
+    '._-27s-', // Song Modification Menu
+    '.XkJho', // Bottom Controls
+    '._aGHQ', // Song Stats
+    '.fqEMR', // Pin Button
+    '.Lf02O:nth-child(2)', // Author Line Contrubutors
+    '.eU82V', // Instrument Menu
+    '.ZoZpf', // Play Strum Button
 ].forEach((selector) => {
 	const el = document.querySelector(selector);
 
@@ -11,9 +24,23 @@
 });
 
 // Check Author Line
-const el = document.querySelector('.relZm')
-if (el && el.textContent.includes('Author Unregistered.')) 	el.remove();
+const elAuthorLine = document.querySelector('.relZm')
+if (elAuthorLine && elAuthorLine.textContent.includes('Author Unregistered.')) 	elAuthorLine.remove();
 
 // Bump Font Size
-const el = document.querySelector('.k_vI3.KLhHx')
-if (el) el.style.fontSize = '20px';
+const elContent = document.querySelector('.k_vI3.KLhHx')
+if (elContent) elContent.style.fontSize = '20px';
+
+// Remove all canvas elements inside each RZayQ
+document.querySelectorAll('.RZayQ').forEach((rzayq) => {
+	rzayq.querySelectorAll('canvas').forEach((canvas) => canvas.remove());
+});
+
+// Cleanup Chords Section
+document.querySelectorAll('.RZayQ').forEach((el) => el.remove());
+document.querySelectorAll('.FlgDy.pvu2n').forEach((el) => {
+    el.style.width = '80px';
+    el.style.padding = '0px';
+});
+const elChordSection = document.querySelector('.lnasI')
+if (elChordSection) elChordSection.style.removeProperty('padding-right');
