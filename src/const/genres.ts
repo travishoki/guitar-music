8c72566e2bdca2c192fd12d3335ba7d44deaab24
Travis Hoki <travis.hoki@gmail.com>
2026-08-06 17:15:09 -0600

    Soul instead of religious for the genres.

--- src/const/genres.ts
+++ src/const/genres.ts
@@ -3,8 +3,8 @@ export const FOLK = 'Folk';
 export const KIDS = 'Kids';
 export const PATRIOTIC = 'Patriotic';
 export const R_AND_B = 'R&B';
-export const RELIGIOUS = 'Religious';
 export const ROCK = 'Rock';
+export const SOUL = 'Soul';
 export const TRADITIONAL = 'Traditional';
 
 export const GENRE_LIST = [
@@ -14,6 +14,6 @@ export const GENRE_LIST = [
 	PATRIOTIC,
 	R_AND_B,
 	ROCK,
-	RELIGIOUS,
+	SOUL,
 	TRADITIONAL,
 ];
