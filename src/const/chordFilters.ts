ba5cc4d6ec721bfe312ca3ba0ab922b172727fc7
Travis Hoki <travis.hoki@gmail.com>
2026-08-06 17:00:01 -0600

    chord filters

---
+++ src/const/chordFilters.ts
@@ -0,0 +1,7 @@
+export const ALL = 'All';
+export const MAJOR = 'Major';
+export const MINOR = 'Minor';
+export const SHARP = 'Sharp';
+export const FLAT = 'Flat';
+
+export const CHORD_FILTER_LIST = [ALL, MAJOR, MINOR, SHARP, FLAT];
--- src/routes/chords/ChordsPage.tsx
+++ src/routes/chords/ChordsPage.tsx
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ChordCard from './ChordCard/ChordCard';
+import SideScrollSelector from '../../components/SideScrollSelector/SideScrollSelector';
 import { ChordList } from '../../const/ChordList';
+import {
+	ALL,
+	CHORD_FILTER_LIST,
+	FLAT,
+	MAJOR,
+	MINOR,
+	SHARP,
+} from '../../const/chordFilters';
+import { ChordType } from '../../types';
 
-const ChordsPage = () => (
-	<div style={gridStyle}>
-		{ChordList.map((chord) => (
-			<ChordCard key={chord.title} src={chord.url} title={chord.title} />
-		))}
-	</div>
-);
+const matchesFilter = (chord: ChordType, filter: string) => {
+	switch (filter) {
+		case MAJOR:
+			return chord.major;
+		case MINOR:
+			return !chord.major;
+		case SHARP:
+			return chord.sharp;
+		case FLAT:
+			return chord.flat;
+		default:
+			return true;
+	}
+};
+
+const ChordsPage = () => {
+	const [filter, setFilter] = useState(ALL);
+
+	const filteredChords = ChordList.filter((chord) =>
+		matchesFilter(chord, filter),
+	);
+
+	return (
+		<>
+			<SideScrollSelector
+				currentOption={filter}
+				list={CHORD_FILTER_LIST}
+				onClick={setFilter}
+			/>
+
+			<div style={gridStyle}>
+				{filteredChords.map((chord) => (
+					<ChordCard key={chord.title} src={chord.url} title={chord.title} />
+				))}
+			</div>
+		</>
+	);
+};
 
 const gridStyle: React.CSSProperties = {
 	display: 'grid',
